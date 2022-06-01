c59c7aef04378acedbeb381a3eef73d0d5996c5f
Dkell88 <DarrenKelly1188@gmail.com>
2022-06-01 16:28:51 -0700

    Create user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  
   namespace :admin do
     get 'categories/index'
   end
@@ -22,6 +23,14 @@ Rails.application.routes.draw do
   end
 
   resources :about, only: [:index]
+
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
