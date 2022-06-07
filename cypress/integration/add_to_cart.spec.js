a26bc639df11ef778b6f7c48e0d4332274e3db45
Dkell88 <DarrenKelly1188@gmail.com>
2022-06-07 15:01:06 -0700

    Added cypress test for add button

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,14 @@
+describe('Testing Cart page', () => {  
+  
+  beforeEach(() => {  
+    cy.visit('/')   
+  }) 
+
+  it("Should navigate to product page", () => {
+    
+    cy.get('.products article').first().click()
+    cy.get('.btn').click()
+    cy.get('.end-0 > .nav-link').should('include.text', 'My Cart (1)')
+   });
+ 
+})  
